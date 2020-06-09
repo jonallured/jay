eea52c5c0bad19b01e5d9714d106c54fdd964232
Jon Allured <jon.allured@gmail.com>
2020-06-09 16:33:35 -0500

    Run prettier over project

--- src/commands/done.ts
+++ src/commands/done.ts
@@ -1,11 +1,11 @@
-import {Command} from '@oclif/command'
+import { Command } from "@oclif/command"
 
 export default class Done extends Command {
-  static description = 'Announce when things are done.'
+  static description = "Announce when things are done."
 
   async run() {
-    const {exec} = require('child_process')
-    exec('basename `pwd`', (_error: any, stdout: string, _stderr: any) => {
+    const { exec } = require("child_process")
+    exec("basename `pwd`", (_error: any, stdout: string, _stderr: any) => {
       const basename = stdout.trim()
       const command = `say ${basename} done -v Fiona && unicornleap --seconds 1`
       exec(command)
--- src/commands/sharpen.ts
+++ src/commands/sharpen.ts
@@ -1,19 +1,19 @@
-import {Command} from '@oclif/command'
-import * as moment from 'moment'
+import { Command } from "@oclif/command"
+import * as moment from "moment"
 
-const os = require('os')
-const {exec} = require('child_process')
+const os = require("os")
+const { exec } = require("child_process")
 
 export default class Sharpen extends Command {
-  static description = 'Create sharpen ticket for this machine.'
+  static description = "Create sharpen ticket for this machine."
 
   async run() {
-    const issueUrl = 'https://github.com/jonallured/dotfiles/issues/new'
-    const template = 'sharpen.md'
-    const label = 'sharpen'
+    const issueUrl = "https://github.com/jonallured/dotfiles/issues/new"
+    const template = "sharpen.md"
+    const label = "sharpen"
 
     const weekNumber = moment().week()
-    const hostname = os.hostname().split('.')[0]
+    const hostname = os.hostname().split(".")[0]
     const title = `Week ${weekNumber} - ${hostname}`
     const encodedTitle = encodeURIComponent(title)
 
