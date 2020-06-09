41ea99a599c2f799a47075143b253c2714ec0b02
Jon Allured <jon.allured@gmail.com>
2020-06-09 16:50:22 -0500

    Setup eslint

--- src/commands/done.ts
+++ src/commands/done.ts
@@ -1,11 +1,11 @@
 import { Command } from "@oclif/command"
+import { exec } from "child_process"
 
 export default class Done extends Command {
   static description = "Announce when things are done."
 
-  async run() {
-    const { exec } = require("child_process")
-    exec("basename `pwd`", (_error: any, stdout: string, _stderr: any) => {
+  async run(): Promise<void> {
+    exec("basename `pwd`", (_error, stdout: string) => {
       const basename = stdout.trim()
       const command = `say ${basename} done -v Fiona && unicornleap --seconds 1`
       exec(command)
--- src/commands/sharpen.ts
+++ src/commands/sharpen.ts
@@ -1,19 +1,18 @@
 import { Command } from "@oclif/command"
 import * as moment from "moment"
-
-const os = require("os")
-const { exec } = require("child_process")
+import { hostname as getHostName } from "os"
+import { exec } from "child_process"
 
 export default class Sharpen extends Command {
   static description = "Create sharpen ticket for this machine."
 
-  async run() {
+  async run(): Promise<void> {
     const issueUrl = "https://github.com/jonallured/dotfiles/issues/new"
     const template = "sharpen.md"
     const label = "sharpen"
 
     const weekNumber = moment().week()
-    const hostname = os.hostname().split(".")[0]
+    const hostname = getHostName().split(".")[0]
     const title = `Week ${weekNumber} - ${hostname}`
     const encodedTitle = encodeURIComponent(title)
 
