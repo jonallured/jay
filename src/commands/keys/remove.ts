57c35a957f973f766417e7074c1bae410ae7c34b
Jon Allured <jon.allured@gmail.com>
2020-08-04 10:34:29 -0500

    Add command to remove keys

---
+++ src/commands/keys/remove.ts
@@ -0,0 +1,28 @@
+import { Command } from "@oclif/command"
+import { Jay } from "../../shared/Jay"
+import { JayUtils } from "../../shared/JayUtils"
+import { computeSafelist } from "../../helpers/keys"
+
+export default class Remove extends Command {
+  static description = "add a user to the safelist (then update)"
+
+  static strict = false
+
+  async run(): Promise<void> {
+    const { safelistFilePath } = Jay.instance.config
+
+    const removeList = this.parse(Remove).argv
+
+    const existingData = JayUtils.readFile(safelistFilePath)
+    const initialList = existingData.split("\n")
+
+    const safeList = computeSafelist({
+      appendList: [],
+      initialList,
+      removeList,
+    })
+    const data = safeList.join("\n")
+
+    JayUtils.writeFile(safelistFilePath, data)
+  }
+}
