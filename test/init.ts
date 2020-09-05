f4a74839c3fd5647a0886d50b89f447a034d8f4d
Jon Allured <jon.allured@gmail.com>
2020-09-05 09:20:41 -0500

    Add some integration tests for keys:init

--- test/init.ts
+++ test/init.ts
@@ -1,5 +1,6 @@
 import { JayConfig } from "../src/shared/JayConfig"
 import * as keysHelpers from "../src/helpers/keys"
+import dirTree from "directory-tree"
 
 JayConfig.init()
 
@@ -9,4 +10,32 @@ export const mockKeysHelper = (helperName: KeysHelpersName): jest.Mock => {
   const mockHelper = jest.fn()
   jest.spyOn(keysHelpers, helperName).mockImplementation(mockHelper)
   return mockHelper
+}
+
+export const root = process.cwd()
+
+interface Tree {
+  type: string
+  size: number
+  name: string
+  children?: Tree[]
+}
+
+const cleanTree = (tree: Tree): Tree => {
+  const { children, name, size, type } = tree
+  const cleaned: Tree = {
+    name,
+    size,
+    type,
+  }
+  if (children) {
+    cleaned.children = children.map((child) => cleanTree(child))
+  }
+  return cleaned
+}
+
+export const captureStructure = (path: string): Tree => {
+  const tree = dirTree(path)
+  const cleanedTree = cleanTree(tree)
+  return cleanedTree
 }
---
+++ test/integration/init.test.ts
@@ -0,0 +1,60 @@
+import { initKeys } from "../../src/helpers/keys"
+import { JayConfig } from "../../src/shared/JayConfig"
+import { JayUtils } from "../../src/shared/JayUtils"
+import { captureStructure, root } from "../init"
+
+const computePath = (testFolder: string): string => {
+  const fixtureFolder = "tmp/test/integration/fixtures"
+  const path = [root, fixtureFolder, testFolder].join("/")
+  return path
+}
+
+beforeAll(() => {
+  const rmCommand = "rm -rf tmp/test"
+  const mkdirCommand = "mkdir -p tmp/test/integration"
+  const cpCommand = "cp -R test/integration/fixtures tmp/test/integration"
+  const commands = [rmCommand, mkdirCommand, cpCommand].join(" && ")
+  JayUtils.exec(commands)
+})
+
+describe("initializing", () => {
+  describe("with no existing authorized keys file", () => {
+    it("sets up an initial keys structure", () => {
+      const path = computePath("no-existing")
+      const config = new JayConfig(path)
+      initKeys(config)
+      const structure = captureStructure(path)
+      expect(structure).toMatchSnapshot()
+    })
+  })
+
+  describe("with an empty existing authorized keys file", () => {
+    it("sets up an initial keys structure", () => {
+      const path = computePath("empty-existing")
+      const config = new JayConfig(path)
+      initKeys(config)
+      const structure = captureStructure(path)
+      expect(structure).toMatchSnapshot()
+    })
+  })
+
+  describe("with some existing authorized keys", () => {
+    it("setups up an inital keys structure and honors those existing keys", () => {
+      const path = computePath("some-existing")
+      const config = new JayConfig(path)
+      initKeys(config)
+      const structure = captureStructure(path)
+      expect(structure).toMatchSnapshot()
+    })
+  })
+
+  describe("with an already setup keys structure", () => {
+    it("does nothing", () => {
+      const path = computePath("already-setup")
+      const config = new JayConfig(path)
+      initKeys(config)
+      const structure = captureStructure(path)
+      expect(structure).toMatchSnapshot()
+    })
+  })
+})
