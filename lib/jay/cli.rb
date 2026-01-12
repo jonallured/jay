ba3847a66c9bd7d8be1fb5d867a8653f1af8e159
Jon Allured <jon@jonallured.com>
2026-01-12 16:18:49 -0600

    Move to using a root command class

--- lib/jay/cli.rb
+++ lib/jay/cli.rb
@@ -1,18 +1,6 @@
 require "thor"
 
 module Jay
-  class Cli < Thor
-    desc "version", "Print the version"
-    def version
-      say Jay::VERSION
-    end
-
-    def self.basename
-      "jay"
-    end
-
-    def self.exit_on_failure?
-      true
-    end
+  module Cli
   end
 end
---
+++ lib/jay/cli/root_command.rb
@@ -0,0 +1,18 @@
+module Jay
+  module Cli
+    class RootCommand < Thor
+      desc "version", "Print the version"
+      def version
+        say Jay::VERSION
+      end
+
+      def self.basename
+        "jay"
+      end
+
+      def self.exit_on_failure?
+        true
+      end
+    end
+  end
+end
